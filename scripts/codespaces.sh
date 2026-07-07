08dab4f3ae262d71463da4de971e293dcef314b4
Sebastian Döll <sebastian@katallaxie.dev>
2026-07-07 14:36:44 +0200

    fix: set default shell zsh

--- scripts/codespaces.sh
+++ scripts/codespaces.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+# Get the path of zsh
+ZSH_PATH=$(which zsh)
+
 # Installing zplug
 export ZPLUG_HOME="$HOME/.zplug"
 [[ ! -d "$ZPLUG_HOME" ]] && git clone 'https://github.com/zplug/zplug' "$ZPLUG_HOME" || true
 
+# Change the default shell to zsh
+chsh -s "$ZSH_PATH"
